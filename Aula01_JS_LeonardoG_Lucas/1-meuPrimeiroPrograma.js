961852b2fd5efa0780b5ab6128834a26a7164428
LeonardoSGas <leonardo.gaspareto@escola.pr.gov.br>
2024-09-06 08:51:29 -0300

    Add files via upload

---
+++ Aula01_JS_LeonardoG_Lucas/1-meuPrimeiroPrograma.js
@@ -0,0 +1,15 @@
+//primeira instrução em js
+console.log("\nOlá mundo cruel da programação\n");
+
+const aluno1="Leonardo";
+const aluno2="Lucas";
+
+const idadeAluno1 = 16;
+const idadeAluno2 = 16;
+
+const somaIdades = idadeAluno1+idadeAluno2
+
+const dupla = aluno1+ " e " +aluno2
+
+console.log("Somos a dupla: " +dupla+"\n");
+console.log("A soma de nossas idades é "+somaIdades+" anos")
